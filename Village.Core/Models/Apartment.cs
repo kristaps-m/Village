6f986f0d9544f5b5670017514b7705948c547c6a
Kristaps-M <kristapsmitins@gmail.com>
2023-01-28 09:15:59 +0200

    Add project files.

---
+++ Village.Core/Models/Apartment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Village.Core.Models
+{
+    public class Apartment
+    {
+        public int Number { get; set; }
+        public int Floor { get; set; }
+        public int NumberOfRooms { get; set; }
+        public int Population { get; set; }
+        public double FullArea { get; set; }
+        public double LivingSpace { get; set; }
+    }
+}
---
+++ Village.Core/Models/House.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Village.Core.Models
+{
+    public class House
+    {
+        public int Number { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Postcode { get; set; }
+    }
+}
