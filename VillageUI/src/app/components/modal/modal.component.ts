c465d1d41b37d9b359aa3127ff28855808cb9202
Kristaps-M <kristapsmitins@gmail.com>
2023-04-01 18:19:22 +0300

    fix modal button, fix edit in modal window

--- VillageUI/src/app/components/modal/modal.component.ts
+++ VillageUI/src/app/components/modal/modal.component.ts
@@ -1,54 +1,54 @@
-import { Component } from '@angular/core';
-import {
-  ModalDismissReasons,
-  NgbDatepickerModule,
-  NgbModal,
-} from '@ng-bootstrap/ng-bootstrap';
-
-@Component({
-  selector: 'app-modal',
-  standalone: true,
-  imports: [NgbDatepickerModule],
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css'],
-})
-export class ModalComponent {
-  closeResult = '';
-
-  constructor(private modalService: NgbModal) {}
-
-  open(content: any) {
-    this.modalService
-      .open(content, { ariaLabelledBy: 'modal-basic-title' })
-      .result.then(
-        (result) => {
-          this.closeResult = `Closed with: ${result}`;
-        },
-        (reason) => {
-          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        }
-      );
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
-}
-
 // import { Component } from '@angular/core';
-// // import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
+// import {
+//   ModalDismissReasons,
+//   NgbDatepickerModule,
+//   NgbModal,
+// } from '@ng-bootstrap/ng-bootstrap';
 
 // @Component({
 //   selector: 'app-modal',
+//   standalone: true,
+//   imports: [NgbDatepickerModule],
 //   templateUrl: './modal.component.html',
 //   styleUrls: ['./modal.component.css'],
 // })
 // export class ModalComponent {
-//   // constructor(public modalRef: MdbModalRef<ModalComponent>) {}
+//   closeResult = '';
+
+//   constructor(private modalService: NgbModal) {}
+
+//   open(content: any) {
+//     this.modalService
+//       .open(content, { ariaLabelledBy: 'modal-basic-title' })
+//       .result.then(
+//         (result) => {
+//           this.closeResult = `Closed with: ${result}`;
+//         },
+//         (reason) => {
+//           this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+//         }
+//       );
+//   }
+
+//   private getDismissReason(reason: any): string {
+//     if (reason === ModalDismissReasons.ESC) {
+//       return 'by pressing ESC';
+//     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+//       return 'by clicking on a backdrop';
+//     } else {
+//       return `with: ${reason}`;
+//     }
+//   }
 // }
+
+import { Component } from '@angular/core';
+//import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent {
+  //constructor(public modalRef: MdbModalRef<ModalComponent>) {}
+}
