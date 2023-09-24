6fabf2da8b300a00a45433982a8f29476073568e
Kristaps-M <kristapsmitins@gmail.com>
2023-09-24 22:38:22 +0300

    add token inside service

--- VillageUI/src/app/services/house-dto.service.ts
+++ VillageUI/src/app/services/house-dto.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -26,12 +26,30 @@ export class HouseDTOService {
         GET ALL HOUSES. BELOW ! ---------------------------------------------------
   */
 
+  // public getHouseDTOs(): Observable<IHouseDTO[]> {
+  //   let x = this.http.get<IHouseDTO[]>(
+  //     `${environment.apiUri}/house/${this.urlGetAll}`
+  //   );
+
+  //   return x;
+  // }
   public getHouseDTOs(): Observable<IHouseDTO[]> {
-    let x = this.http.get<IHouseDTO[]>(
-      `${environment.apiUri}/house/${this.urlGetAll}`
+    // Get the JWT token from your authentication service or storage
+    const token = 'YOUR_JWT_TOKEN';
+    // Replace with your actual token retrieval logic
+
+    // Add the authorization header with the bearer token
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get<IHouseDTO[]>(
+      `${environment.apiUri}/house/${this.urlGetAll}`,
+      {
+        headers: headers,
+      }
     );
-    
-    return x;
   }
   // update, create, delete ... from IHouseDTO[] to IHouseDTO
   public updateHouseDTOs(house: IHouseDTO): Observable<IHouseDTO> {
@@ -39,13 +57,13 @@ export class HouseDTOService {
       `${environment.apiUri}/house/${this.toUpdate}`,
       house
     );
-    
+
     return x;
   }
 
   public createHouseDTOs(house: IHouseDTO): Observable<IHouseDTO> {
     let x = this.http.post<IHouseDTO>(`${environment.apiUri}/house/add`, house);
-    
+
     return x;
   }
 
@@ -53,7 +71,7 @@ export class HouseDTOService {
     let x = this.http.delete<IHouseDTO>(
       `${environment.apiUri}/house/${house.id}`
     );
-    
+
     return x;
   }
 }
